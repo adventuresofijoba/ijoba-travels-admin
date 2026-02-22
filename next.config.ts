ab3a0cd695a371c71497d4df269b28d94ea7c74f
Abraham <devsenpai09@gmail.com>
2026-02-23 00:30:13 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: "https",
-        hostname: "zwtzncewhigoqzforzjh.supabase.co",
+        hostname: "dhuahskektxsztgyetnl.supabase.co",
       },
     ],
   },
